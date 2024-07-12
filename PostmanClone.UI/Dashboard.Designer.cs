2b56aa93815b08cc822e4beb28c8506c9750ca8e
Albin Nilsson af Sillén <albin.nilsson.afsillen@compago.se>
2024-07-12 15:08:17 +0200

    Added quick librarycall

--- PostmanClone.UI/Dashboard.Designer.cs
+++ PostmanClone.UI/Dashboard.Designer.cs
@@ -28,20 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
+            apiLabel = new Label();
+            apiText = new TextBox();
+            resultWindow = new TextBox();
+            callAPI = new Button();
+            statusStrip = new StatusStrip();
+            stripStatus = new ToolStripStatusLabel();
+            resultsLabel = new Label();
+            statusStrip.SuspendLayout();
             SuspendLayout();
+            // 
+            // apiLabel
+            // 
+            apiLabel.AutoSize = true;
+            apiLabel.Location = new Point(42, 21);
+            apiLabel.Name = "apiLabel";
+            apiLabel.Size = new Size(127, 39);
+            apiLabel.TabIndex = 1;
+            apiLabel.Text = "API Url:";
+            // 
+            // apiText
+            // 
+            apiText.BackColor = SystemColors.Info;
+            apiText.Location = new Point(42, 63);
+            apiText.Multiline = true;
+            apiText.Name = "apiText";
+            apiText.Size = new Size(768, 104);
+            apiText.TabIndex = 2;
+            // 
+            // resultWindow
+            // 
+            resultWindow.BackColor = SystemColors.Info;
+            resultWindow.BorderStyle = BorderStyle.FixedSingle;
+            resultWindow.Location = new Point(42, 229);
+            resultWindow.Multiline = true;
+            resultWindow.Name = "resultWindow";
+            resultWindow.ReadOnly = true;
+            resultWindow.ScrollBars = ScrollBars.Both;
+            resultWindow.Size = new Size(768, 439);
+            resultWindow.TabIndex = 3;
+            // 
+            // callAPI
+            // 
+            callAPI.BackColor = SystemColors.Info;
+            callAPI.Location = new Point(854, 92);
+            callAPI.Name = "callAPI";
+            callAPI.Size = new Size(112, 47);
+            callAPI.TabIndex = 4;
+            callAPI.Text = "GO";
+            callAPI.UseVisualStyleBackColor = false;
+            callAPI.Click += callAPI_Click;
+            // 
+            // statusStrip
+            // 
+            statusStrip.BackColor = SystemColors.Info;
+            statusStrip.ImageScalingSize = new Size(24, 24);
+            statusStrip.Items.AddRange(new ToolStripItem[] { stripStatus });
+            statusStrip.Location = new Point(0, 696);
+            statusStrip.Name = "statusStrip";
+            statusStrip.Size = new Size(1013, 40);
+            statusStrip.TabIndex = 5;
+            statusStrip.Text = "statusStrip1";
+            // 
+            // stripStatus
+            // 
+            stripStatus.BackColor = SystemColors.Info;
+            stripStatus.Font = new Font("Comic Sans MS", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            stripStatus.Name = "stripStatus";
+            stripStatus.Size = new Size(82, 33);
+            stripStatus.Text = "Ready";
+            // 
+            // resultsLabel
+            // 
+            resultsLabel.AutoSize = true;
+            resultsLabel.Location = new Point(42, 187);
+            resultsLabel.Name = "resultsLabel";
+            resultsLabel.Size = new Size(122, 39);
+            resultsLabel.TabIndex = 6;
+            resultsLabel.Text = "Results:";
             // 
             // Dashboard
             // 
             AutoScaleDimensions = new SizeF(17F, 39F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1360, 702);
+            BackColor = Color.Salmon;
+            ClientSize = new Size(1013, 736);
+            Controls.Add(resultsLabel);
+            Controls.Add(statusStrip);
+            Controls.Add(callAPI);
+            Controls.Add(resultWindow);
+            Controls.Add(apiText);
+            Controls.Add(apiLabel);
             Font = new Font("Comic Sans MS", 14F);
-            Margin = new Padding(5, 5, 5, 5);
+            Margin = new Padding(5);
             Name = "Dashboard";
-            Text = "Postman Clone";
+            Text = "Dashboard";
+            statusStrip.ResumeLayout(false);
+            statusStrip.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Label apiLabel;
+        private TextBox apiText;
+        private TextBox resultWindow;
+        private Button callAPI;
+        private StatusStrip statusStrip;
+        private Label resultsLabel;
+        private ToolStripStatusLabel stripStatus;
     }
 }
--- PostmanClone.UI/Dashboard.cs
+++ PostmanClone.UI/Dashboard.cs
@@ -1,3 +1,5 @@
+using PostmanClone.Library;
+
 namespace PostmanClone.UI
 {
     public partial class Dashboard : Form
@@ -5,6 +7,36 @@ namespace PostmanClone.UI
         public Dashboard()
         {
             InitializeComponent();
+        }
+
+        private async void callAPI_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (string.IsNullOrEmpty(apiText.Text.ToLower()) || !apiText.Text.StartsWith("https://"))
+                {
+                    stripStatus.Text = "Error in Api Url. Must contain value and start with https://";
+                    return;
+                }
+                stripStatus.Text = "Calling API...";
+
+                PostManLibrary library = new PostManLibrary();
+
+                int callType = 1;
+                var res = await library.Call(apiText.Text, callType);
+
+
+
+                await Task.Delay(2000);
+
+                stripStatus.Text = "Ready";
+            }
+            catch (Exception ex)
+            {
+                resultWindow.Text = "Error happend" + ex.Message;
+                stripStatus.Text = "Error";
+            }
         }
     }
 }
