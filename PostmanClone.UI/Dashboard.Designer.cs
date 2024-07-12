c0a611ec6087523e75986db67ec0d66faa6dcdc9
Albin Nilsson af Sillén <albin.nilsson.afsillen@compago.se>
2024-07-12 13:52:35 +0200

    Renamed to Dashboard

--- PostmanClone.UI/Dashboard.Designer.cs
+++ PostmanClone.UI/Dashboard.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PostmanClone.UI
 {
-    partial class Form1
+    partial class Dashboard
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // Dashboard
+            // 
+            AutoScaleDimensions = new SizeF(17F, 39F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1360, 702);
+            Font = new Font("Comic Sans MS", 14F);
+            Margin = new Padding(5, 5, 5, 5);
+            Name = "Dashboard";
+            Text = "Postman Clone";
+            ResumeLayout(false);
         }
 
         #endregion
